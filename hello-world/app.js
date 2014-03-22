070e6ab8f93b0b70ee3b26f10c333fb0268814d8
swapnil-bawkar <sopu10@gmail.com>
2014-03-23 01:13:28 +0530

    Added json Get request

--- hello-world/app.js
+++ hello-world/app.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });
 
+app.get('/json', function(req, res){
+  res.send({msg:'Hello World'});
+});
+
 var server = app.listen(3000, function(){
 	console.log('Listening on port %d', server.address().port);
 });
